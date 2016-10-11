edaca49c30274e8aeb1d5ee425797bbe7401b232
Hongying LIU <dadaycm@hotmail.com>
2016-10-11 09:36:11 +0800

    Add model: User

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all()
+User.create!([{name: "Xiexie"}, {email: "caixieen@liu.com"}] )
+User.create!([{name: "Feifei"}, {email: "feifei@liu.com"}] )
+User.create!([{name: "Yaoyao"}, {email: "yaoyao@liu.com"}] )
+User.create!([{name: "Henhen"}, {email: "henhen@liu.com"}] )
